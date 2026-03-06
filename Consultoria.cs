7e786fddeeeb5934ff354f3ad9e46c9345e557ca
Hiago 7k <hiago7k@gmail.com>
2026-03-05 21:39:26 -0300

    criando classe Consultoria com composicao e Interface

--- Consultoria.cs
+++ Consultoria.cs
@@ -3,13 +3,22 @@
 internal class Consultoria : IServico
 {
     public Funcionario colaborador;
+
+    public Consultoria(Funcionario colaborador, string tipoDeServico, string tituloDaTarefa)
+    {
+        this.colaborador = colaborador;
+        TipoDeServico = tipoDeServico;
+        TituloDaTarefa = tituloDaTarefa;
+    }
+
     public string TipoDeServico { get; }
     public string TituloDaTarefa { get; }
 
 
     public void ExecutarServico() 
     {
-        Console.WriteLine($"Executando servico de consultoria: {}");
+        Console.WriteLine($"Executando servico de consultoria: {TituloDaTarefa} {TipoDeServico}");
+        Console.WriteLine($"Responsavel: {colaborador.Nome} {colaborador.Departamento}");
     }
 
 }
