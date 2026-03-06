3ede7815b9e57cb79ef5d45c6c2aafb6ec2c1134
Hiago 7k <hiago7k@gmail.com>
2026-03-05 21:37:47 -0300

    criando propriedades

--- Consultoria.cs
+++ Consultoria.cs
@@ -2,4 +2,14 @@
 
 internal class Consultoria : IServico
 {
+    public Funcionario colaborador;
+    public string TipoDeServico { get; }
+    public string TituloDaTarefa { get; }
+
+
+    public void ExecutarServico() 
+    {
+        Console.WriteLine($"Executando servico de consultoria: {}");
+    }
+
 }
